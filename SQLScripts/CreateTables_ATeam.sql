8be4015f0d82040c2243f9ee18a38812bc48a155
Stephen McGuigan <stephen.mcguigan@kainos.com>
2021-08-26 12:21:20 +0100

    Added SQLScripts

---
+++ SQLScripts/CreateTables_ATeam.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE Kainos_A;
+
+use Kainos_A;
+
+CREATE TABLE `roles` (
+  `roleID` smallint NOT NULL AUTO_INCREMENT,
+  `roleName` varchar(100) NOT NULL,
+  `roleDesc` text NOT NULL,
+  `datePosted` date not null,
+  `hours` varchar(10) not null,
+  `location` varchar(100) not null,
+  `bandID` tinyint NOT NULL,
+  `capabilityID` tinyint NOT NULL,
+  PRIMARY KEY (`roleID`),
+  CONSTRAINT `band_to_role_FK` FOREIGN KEY (`bandID`) REFERENCES `band` (`bandID`),
+  CONSTRAINT `capability_to_role_FK` FOREIGN KEY (`capabilityID`) REFERENCES `capability` (`capabilityID`)
+);
+
+CREATE TABLE `capability` (
+  `capabilityID` tinyint NOT NULL AUTO_INCREMENT,
+  `capability` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`capabilityID`)
+);
+
+CREATE TABLE `band` (
+  `bandID` tinyint NOT NULL AUTO_INCREMENT,
+  `bandName` varchar(40) DEFAULT NULL,
+  `bandExpectations` text,
+  PRIMARY KEY (`bandID`)
+);
