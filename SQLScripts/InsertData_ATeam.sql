6e97cc06cad35d44437ccf7d62ef469d5bc64e55
KristinaGeddis <kristina.geddis@kainos.com>
2021-09-02 15:48:48 +0100

    formatted the date to be mm/dd/yy

--- SQLScripts/InsertData_ATeam.sql
+++ SQLScripts/InsertData_ATeam.sql
@@ -6,7 +6,7 @@ insert into capability (capability) VALUES(("Engineering"),
 											("Platforms"));
 
 INSERT INTO roles (`roleName`, `roleDesc`, `datePosted`, `hours`, `location`, `bandID`, `capabilityID`)
-VALUES ("Product Principal", "PlaceHolder", "2021-07-13", "Full Time", "Birmingham", 1, 1),
-		("Solutions Architect", "PlaceHolder", "2021-06-20", "Full Time", "Derry-Londonderry", 1, 1),
-        ("Market Intelligence Analyst", "PlaceHolder", "2021-08-24", "Full Time", "London", 1, 1), 
-        ("Market Intelligence Analyst", "PlaceHolder", "2021-01-24", "Full Time", "Birmingham", 1, 2);
+VALUES ("Product Principal", "PlaceHolder", STR_TO_DATE('06-01-2019', '%m-%d-%Y'), "Full Time", "Birmingham", 1, 1),
+		("Solutions Architect", "PlaceHolder", STR_TO_DATE('2021-06-20', '%m-%d-%Y'), "Full Time", "Derry-Londonderry", 1, 1),
+        ("Market Intelligence Analyst", "PlaceHolder", STR_TO_DATE('2021-08-25', '%m-%d-%Y'), "Full Time", "London", 1, 1), 
+        ("Market Intelligence Analyst", "PlaceHolder", STR_TO_DATE('2021-01-04', '%m-%d-%Y'), "Full Time", "Birmingham", 1, 2);
