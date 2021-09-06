cfec52388c295804a013443cc64c5c2475ce5d72
Ruxandra Stanciu <ruxandra.stanciu@kainos.com>
2021-09-06 14:44:55 +0100

    006 review 3

--- backend/JavaWebService/src/main/java/com/kainos/ea/db/RoleMapper.java
+++ backend/JavaWebService/src/main/java/com/kainos/ea/db/RoleMapper.java
@@ -7,11 +7,11 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface RoleMapper {
-    @Select("" +
-            "SELECT r.roleID, r.roleName, r.roleDesc, r.datePosted, r.hours, r.location, r.bandID, r.capabilityId, r.link," +
+    @Select("SELECT r.roleID, r.roleName, r.roleDesc, r.datePosted, r.hours, r.location, r.bandID, r.capabilityId, r.link," +
             "group_concat(re.responsibility) as jobResponsibilities FROM roles r" +
             " LEFT JOIN role_responsibilities rr ON r.roleID = rr.roleId" +
-            " LEFT JOIN responsibilities re ON rr.responsibilityId = re.responsibilityId WHERE r.roleID = #{roleID}" +
+            " LEFT JOIN responsibilities re ON rr.responsibilityId = re.responsibilityId" +
+            " WHERE r.roleID = #{roleID}" +
             " GROUP BY " + "r.roleID;")
     Role getRole(int roleID);
 
