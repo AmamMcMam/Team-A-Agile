f27108c2f786d367c026f680251bf4cf2612dd34
Amir Makanvand <amirmakanvand@hotmail.com>
2021-09-06 12:25:11 +0100

    Update InsertData_ATeam.sql
    Insert roles fixed

--- SQLScripts/InsertData_ATeam.sql
+++ SQLScripts/InsertData_ATeam.sql
@@ -8,19 +8,22 @@ insert into capability (capability) VALUES(("Engineering"),
 											("Platforms"));
 
 INSERT INTO roles (`roleName`, `roleDesc`, `datePosted`, `hours`, `location`, `bandID`, `capabilityID`, `link`)
+VALUES ("Product Principal", "PlaceHolder", "13-07-2021", "Full Time", "Birmingham", 1, 1, ""),
+        ("Solutions Architect",
+         "As a Solution Architect (Manager) in Kainos, you’ll be responsible for leading multi-skilled agile teams to design and deliver high quality solutions which delight our customers and impact the lives of users worldwide. You’ll work with customer architects to agree functional and non-functional designs, advising customers and managers on the estimated effort, technical implications and complexity surrounding your designs.",
+          "20-06-2021", "Full Time", "Derry-Londonderry", 1, 1, "https://kainossoftwareltd.sharepoint.com/people/Job%20Specifications/Forms/AllItems.aspx?id=%2Fpeople%2FJob%20Specifications%2FEngineering%2FJob%20Profile%20%2D%20Solution%20Architect%20%28Manager%29%2Epdf&parent=%2Fpeople%2FJob%20Specifications%2FEngineering&p=true&originalPath=aHR0cHM6Ly9rYWlub3Nzb2Z0d2FyZWx0ZC5zaGFyZXBvaW50LmNvbS86YjovZy9wZW9wbGUvRVNwcDU2Zlp5cmhLaTZ1Q3V3cHNoRHdCM25tbzFCTWtiUUtZRFhiTkhDaV9zdz9ydGltZT1tcHlFbzJSczJVZw"),
+        ("Market Intelligence Analyst", "PlaceHolder", "24-08-2021", "Full Time", "London", 1, 1, ""),
+        ("Market Intelligence Analyst", "PlaceHolder", "24-01-2021", "Full Time", "Birmingham", 2, 2, "");
 
-VALUES ("Product Principal", "PlaceHolder", "2021-07-13", "Full Time", "Birmingham", 1, 1, ""),
-		("Solutions Architect",
-		 "As a Solution Architect (Manager) in Kainos, you’ll be responsible for leading multi-skilled agile teams to design and deliver high quality solutions which delight our customers and impact the lives of users worldwide. You’ll work with customer architects to agree functional and non-functional designs, advising customers and managers on the estimated effort, technical implications and complexity surrounding your designs.",
-		  "2021-06-20", "Full Time", "Derry-Londonderry", 1, 1, "https://kainossoftwareltd.sharepoint.com/people/Job%20Specifications/Forms/AllItems.aspx?id=%2Fpeople%2FJob%20Specifications%2FEngineering%2FJob%20Profile%20%2D%20Solution%20Architect%20%28Manager%29%2Epdf&parent=%2Fpeople%2FJob%20Specifications%2FEngineering&p=true&originalPath=aHR0cHM6Ly9rYWlub3Nzb2Z0d2FyZWx0ZC5zaGFyZXBvaW50LmNvbS86YjovZy9wZW9wbGUvRVNwcDU2Zlp5cmhLaTZ1Q3V3cHNoRHdCM25tbzFCTWtiUUtZRFhiTkhDaV9zdz9ydGltZT1tcHlFbzJSczJVZw"),
-        ("Market Intelligence Analyst", "PlaceHolder", "2021-08-24", "Full Time", "London", 1, 1, ""), 
-        ("Market Intelligence Analyst", "PlaceHolder", "2021-01-24", "Full Time", "Birmingham", 2, 2, "");
+
 
 INSERT INTO responsibilities (`responsibilityId`, `responsibility`)
 VALUES (1,"Has successfully delivered software designs for multi-tiered modern software applications."),
 		(2,"Understands whole solution architecture concepts and can communicate and negotiate these with customer architects."),
 		(3,"Can prioritise non-functional concerns for customers and has experience incorporating these into the application design."),
 		(4,"Able to simply and clearly communicate technical design in conversation and presentations.");
+
+
 
 INSERT INTO role_responsibilities (`roleId`, `responsibilityId`)
 VALUES (1, 1),
@@ -32,12 +35,7 @@ VALUES (1, 1),
 	   (4, 1),
 	   (4, 3);
 
-VALUES ("Product Principal", "PlaceHolder", "13-07-2021", "Full Time", "Birmingham", 1, 1, ""),
-        ("Solutions Architect",
-         "As a Solution Architect (Manager) in Kainos, you’ll be responsible for leading multi-skilled agile teams to design and deliver high quality solutions which delight our customers and impact the lives of users worldwide. You’ll work with customer architects to agree functional and non-functional designs, advising customers and managers on the estimated effort, technical implications and complexity surrounding your designs.",
-          "20-06-2021", "Full Time", "Derry-Londonderry", 1, 1, "https://kainossoftwareltd.sharepoint.com/people/Job%20Specifications/Forms/AllItems.aspx?id=%2Fpeople%2FJob%20Specifications%2FEngineering%2FJob%20Profile%20%2D%20Solution%20Architect%20%28Manager%29%2Epdf&parent=%2Fpeople%2FJob%20Specifications%2FEngineering&p=true&originalPath=aHR0cHM6Ly9rYWlub3Nzb2Z0d2FyZWx0ZC5zaGFyZXBvaW50LmNvbS86YjovZy9wZW9wbGUvRVNwcDU2Zlp5cmhLaTZ1Q3V3cHNoRHdCM25tbzFCTWtiUUtZRFhiTkhDaV9zdz9ydGltZT1tcHlFbzJSczJVZw"),
-        ("Market Intelligence Analyst", "PlaceHolder", "24-08-2021", "Full Time", "London", 1, 1, ""),
-        ("Market Intelligence Analyst", "PlaceHolder", "24-01-2021", "Full Time", "Birmingham", 2, 2, "");
+
 
 INSERT INTO competency(competencyName, competencyDescription)
 VALUES ("Personal Performance", "Effective leadership requires individuals to draw upon values, strengths, and abilities to deliver high standards of service. Demonstrating effectiveself-awareness, self-management, continued personal development and acting with integrity."),
